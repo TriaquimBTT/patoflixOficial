0bcebe25e3fc6fc2c3713e350bb3bd35f56e43ea
Gustavo Triaquim <gustavotriaquim6@gmail.com>
2024-07-23 16:50:58 -0300

    commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,59 +23,37 @@ function Home() {
 
   return (
     <PageDefault paddingAll={0}>
-      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
+      {/* {dadosIniciais.length === 0 && (<div>Loading...</div>)} */}
 
-      {dadosIniciais.map((categoria, indice) => {
-        if (indice === 0) {
-          return (
-            <div key={categoria.id}>
-              <BannerMain
-                videoTitle={dadosIniciais[0].videos[0].titulo}
-                url={dadosIniciais[0].videos[0].url}
-                videoDescription={dadosIniciais[0].videos[0].description}
-              />
-              <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais[0]}
-              />
-            </div>
-          );
-        }
-
-        return (
-          <Carousel
-            key={categoria.id}
-            category={categoria}
+      {dadosIniciais.length > 0 && (
+        <div>
+          <BannerMain
+            videoTitle={dadosIniciais[0].videos[0].titulo}
+            url={dadosIniciais[0].videos[0].url}
+            videoDescription={dadosIniciais[0].videos[0].description}
           />
-        );
-      })}
+          <Carousel
+            ignoreFirstVideo
+            category={dadosIniciais[0]}
+          />
 
-      {/* <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="O que"
-      />
+          <Carousel
+            category={dadosIniciais[1]}
+          />
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+          <Carousel
+            category={dadosIniciais[2]}
+          />
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+          <Carousel
+            category={dadosIniciais[3]}
+          />
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      /> */}
+          <Carousel
+            category={dadosIniciais[4]}
+          />
+        </div>
+      )}
     </PageDefault>
   );
 }
